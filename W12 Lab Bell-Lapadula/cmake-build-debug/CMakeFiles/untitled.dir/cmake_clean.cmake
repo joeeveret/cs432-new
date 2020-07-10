e160b133082e781162d3bd0f12038c76da0b40f4
Gardner-Kyle <kb_gardner@icloud.com>
2020-07-10 15:53:05 -0600

    Add files via upload

---
+++ W12 Lab Bell-Lapadula/cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/interact.cpp.o"
+  "CMakeFiles/untitled.dir/main.cpp.o"
+  "CMakeFiles/untitled.dir/message.cpp.o"
+  "CMakeFiles/untitled.dir/messages.cpp.o"
+  "untitled"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ W12 Lab Bell-Lapadula/control.h
@@ -0,0 +1,21 @@
+//
+// Created by Kyle Gardner on 7/10/20.
+//
+
+#ifndef UNTITLED_CONTROL_H
+#define UNTITLED_CONTROL_H
+
+/***********************************************************************
+ * COMPONENT:
+ *    CONTROL
+ * Author:
+ *    Br. Helfrich, <your name here if you made a change>
+ * Summary:
+ *    This class stores the notion of Bell-LaPadula
+ ************************************************************************/
+
+#pragma once
+
+// you may need to put something here...
+
+#endif //UNTITLED_CONTROL_H

ea408b28065f02daa61b92d4e3d1da618df29fbf
joeeveret <joeeveret@gmail.com>
2020-07-15 17:22:23 -0600

    Update cipher02.h
    Adding Cipher02

--- Encryption/cipher02.h
+++ Encryption/cipher02.h
@@ -1,6 +1,11 @@
 //
-// Created by Kyle Gardner on 7/14/20.
+//  main.cpp
+//  Cipher02
 //
+//  Created by Joseph Everett on 7/15/20.
+//  Copyright © 2020 Joseph Everett. All rights reserved.
+//
+
 
 /********************************************************************
 * Header:
@@ -19,10 +24,10 @@
 class Cipher02 : public Cipher
 {
 public:
-    virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-    virtual std::string getCipherName()  { return "cipher name"; }
-    virtual std::string getEncryptAuth() { return "encrypt author"; }
-    virtual std::string getDecryptAuth() { return "decrypt author"; }
+    virtual std::string getPseudoAuth()  { return "Joe Everett"; }
+    virtual std::string getCipherName()  { return "Railfence Cipher"; }
+    virtual std::string getEncryptAuth() { return "Joe Everett"; }
+    virtual std::string getDecryptAuth() { return "Joe Everett"; }
 
     /***********************************************************
      * GET CIPHER CITATION
@@ -30,7 +35,7 @@ public:
      ***********************************************************/
     virtual std::string getCipherCitation()
     {
-        return std::string("citation");
+        return std::string("Crypto. (n.d.). Retrieved July 15, 2020, from http://practicalcryptography.com/ciphers/classical-era/rail-fence");
     }
 
     /**********************************************************
@@ -43,10 +48,16 @@ public:
 
         // TODO: please format your pseudocode
         // The encrypt pseudocode
-        str =  "insert the encryption pseudocode\n";
+        str = "1. Prompt user for plaintext \n";
+        str += "2. Prompt user for password. \n";
+        str += "3. Prompt user for number of rows < 10. \n";
+        str += "4. Construct ciphertext using plaintext and password. \n";
+        str += "5. Display ciphertext to user. \n";
+        
 
         // The decrypt pseudocode
-        str += "insert the decryption pseudocode\n";
+        str += "6. Construct plaintext from ciphertext \n";
+        str += "7. Display reconstructed plaintext to user.";
 
         return str;
     }
@@ -59,9 +70,34 @@ public:
                                 const std::string & password)
     {
         std::string cipherText = plainText;
-        // TODO - Add your code here
-        return cipherText;
-    }
+        int key = (int)(cipherText.length()/password.length());
+        char railfence[key][cipherText.length()];
+        
+        for (int i=0; i < key; i++)
+        for (int j = 0; j < cipherText.length(); j++)
+            railfence[i][j] = '\n';
+        
+        bool dir_down = false;
+        int row = 0, col = 0;
+        
+        for (int i=0; i < cipherText.length(); i++)
+        {
+            if (row == 0 || row == key-1)
+                dir_down = !dir_down;
+        
+        railfence[row][col++] = cipherText[i];
+        
+        dir_down?row++ : row--;
+        }
+        
+        std::string result = "";
+        for (int i=0; i < key; i++)
+                for (int j=0; j < cipherText.length(); j++)
+                    if (railfence[i][j]!='\n')
+                        result.push_back(railfence[i][j]);
+          
+            return result;
+        }
 
     /**********************************************************
      * DECRYPT
@@ -71,8 +107,58 @@ public:
                                 const std::string & password)
     {
         std::string plainText = cipherText;
-        // TODO - Add your code here
-        return plainText;
+        int key = (int)(cipherText.length()/password.length());
+        
+        char railfence[key][cipherText.length()];
+        
+        for (int i=0; i < key; i++)
+               for (int j=0; j < cipherText.length(); j++)
+                   railfence[i][j] = '\n';
+       
+        bool dir_down;
+        
+        int row = 0, col = 0;
+        
+        for (int i=0; i < cipherText.length(); i++)
+        {
+            // check the direction of flow
+            if (row == 0)
+                dir_down = true;
+            if (row == key-1)
+                dir_down = false;
+        
+        
+            railfence[row][col++] = '*';
+
+            dir_down?row++ : row--;
+        }
+        
+        int index = 0;
+        for (int i=0; i<key; i++)
+            for (int j=0; j<cipherText.length(); j++)
+                if (railfence[i][j] == '*' && index<cipherText.length())
+                    railfence[i][j] = cipherText[index++];
+        
+        std:: string result = "";
+        
+        row = 0, col = 0;
+        for (int i=0; i< cipherText.length(); i++)
+          {
+              // check the direction of flow
+              if (row == 0)
+                  dir_down = true;
+              if (row == key-1)
+                  dir_down = false;
+        
+              // place the marker
+              if (railfence[row][col] != '*')
+                  result.push_back(railfence[row][col++]);
+        
+              // find the next row using direction flag
+              dir_down?row++: row--;
+          }
+          return result;
+        
     }
 };
 
